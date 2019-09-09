e6b45b0ce5b2df68a6d7d61d436ebba08759cc25
Zainab Siddiqui <zainabsiddiqui8@gmail.com>
2019-09-08 21:06:53 -0400

    Got the bot to stop replying to itself

--- reddit_bot.py
+++ reddit_bot.py
@@ -1,6 +1,7 @@
 import praw 
 import config
 import time
+import os
 
 def bot_login():
 	print("Logging in...")
@@ -19,7 +20,7 @@ def run_bot(r):
 	print("Obtaining 25 comments...")
 
 	for comment in r.subreddit('test').comments(limit=25):
-		if "dog" in comment.body:
+		if "dog" in comment.body and comment.id not in comments_replied_to and comment.author != r.user.me():
 			print("String with 'dog' found in comment " + comment.id)
 
 			comment.reply("I also love dogs! [Here](https://i.imgur.com/W2XqgxI.gifv)"
@@ -27,11 +28,26 @@ def run_bot(r):
 
 			print("Replied to comment " + comment.id)
 
+			comments_replied_to.append(comment.id)
+
+	print(comments_replied_to)
+
 	print("Sleeping for 10 seconds...")
-	
 	#Sleep for 10 seconds...
 	time.sleep(10)
 
+def get_saved_comments():
+	if not os.path.isfile("comments_replied_to.txt"):
+		comments_replied_to = []
+	else:
+		with open("comments_replied_to.txt", "r") as f:
+			comments_replied_to = f.read()
+			comments_replied_to = comments_replied_to.split("\n")
+	return comments_replied_to
+
+r = bot_login()
+comments_replied_to = get_saved_comments()
+print(comments_replied_to)
+
 while True:
-	r = bot_login()
 	run_bot(r)
