78dd8edf8daed9738ddc17554f9768965901cbff
Zainab Siddiqui <zainabsiddiqui8@gmail.com>
2019-09-16 12:09:42 -0400

    Added cleaner comments

--- reddit_bot.py
+++ reddit_bot.py
@@ -5,56 +5,83 @@ import os
 import requests
 
 def bot_login():
+
 	print("Logging in...")
 
+	# Log in to Reddit using my configuration
 	r = praw.Reddit(username = config.username, 
 			password = config.password,
 			client_id = config.client_id,
 			client_secret = config.client_secret,
-			user_agent = "lacetrim's joke comment responder v0.1")
+			user_agent = "lacetrim's boredom responder v0.1")
 
 	print("Logged in!")
 
 	return r
 
 def run_bot(r):
+
 	print("Obtaining 25 comments...")
 
 	for comment in r.subreddit('test').comments(limit=25):
-		if ("I'm bored" in comment.body or "I am bored" in comment.body) and (comment.id not in comments_replied_to) and (comment.author != r.user.me()):
+
+		#If the comment contains a statement of boredom ("I'm bored" or "I am bored"), reply back with a random suggestion
+		if ("I'm bored" in comment.body or "I am bored" in comment.body or "!boredombot" in comment.body) and (comment.id not in comments_replied_to) and (comment.author != r.user.me()):
+
 			print("String with boredom found in comment " + comment.id)
 
+			#Append boilerplate comment with random suggestion
 			comment_reply = "Oh no, there's so much you can do! Here's an idea: "
-
 			suggestion = requests.get('http://www.boredapi.com/api/activity/').json()['activity']
+			comment_reply += suggestion + ".\n\n^(Beep, boop. I am a bot. Comment '!boredombot 3' - or any other number - to get suggestions tailored for that number of participants.)"
 
-			comment_reply += suggestion + ".\n\n^(Beep, boop. I am a bot. Comment '!boredombot 3' - or any other number - to get suggestions tailored for that number of recipients.)"
-
-			# comment_reply += "\n\nThis joke came from [ICNDB.com](http://icndb.com)"
-
+			#Reply to the comment
 			comment.reply(comment_reply)
-
 			print("Replied to comment " + comment.id)
 
+			#Add comment id to .txt file so that we don't reply to it again
 			comments_replied_to.append(comment.id)
 
 			with open("comments_replied_to.txt", "a") as f:
 				f.write(comment.id + "\n")
+
+		elif "!boredombot" in comment.body and comment.id not in comments_replied_to and comment.author != r.user.me():
+
+				number_of_participants = comment.strip()[-1]
+
+				if number_of_participants >= 1:
+					participant_reply = "Here's an idea to pass the time with " + number_of_participants + " people: "
+
+				else:
+					participant_reply = "Uh oh! That's not a valid number of participants."
+
+				participant_url = "http://www.boredapi.com/api/activity?participants=" + number_of_participants
+
+				suggestion = requests.get(participant_url).
+
+				comment.reply(participant_reply)
 
 	print("Sleeping for 10 seconds...")
 	#Sleep for 10 seconds...
 	time.sleep(10)
 
 def get_saved_comments():
+
 	if not os.path.isfile("comments_replied_to.txt"):
+
+		#If the .txt file does not exist already, create a blank one
 		comments_replied_to = []
+
 	else:
+
+		#Otherwise read and parse the file 
 		with open("comments_replied_to.txt", "r") as f:
 			comments_replied_to = f.read()
 			comments_replied_to = comments_replied_to.split("\n")
 			comments_replied_to = list(filter(None, comments_replied_to))
 			
 	return comments_replied_to
+
 
 r = bot_login()
 comments_replied_to = get_saved_comments()
